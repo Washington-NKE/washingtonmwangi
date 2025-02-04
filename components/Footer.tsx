b67ad19fdcb14a53effa6cda94dce87e7a97873d
Washington <washiem2022@gmail.com>
2025-02-05 01:38:22 +0300

    debug

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import { socialMedia } from '@/data'
 
 const Footer = () => {
   return (
-    <footer className="w-full pb-10 mb-[100px] md:mb-2 " id="contact">
+    <div className="w-full pb-10 mb-[100px] md:mb-2 " id="contact">
         <div className='w-full absolute left-0 -bottom-72 min-h-96'>
             <img
             src='/footer-grid.svg'
@@ -33,12 +33,12 @@ const Footer = () => {
             <div className="flex items-center md:gap-3 gap-6">
             {socialMedia.map((profile)=>(
                 <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
-                    <img src={profile.img} alt={profile.} width={20} height={20} />
+                    <img src={profile.img} alt={profile.img} width={20} height={20} />
                 </div>
             ))}
         </div>
         </div>      
-    </footer>
+    </div>
   )
 }
 
