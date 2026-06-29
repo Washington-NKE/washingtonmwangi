c88b12ce5a303cf077ff448b53b0b4c33091ca34
Washington <washiem2022@gmail.com>
2026-06-29 17:26:39 +0300

    Upgrade portfolio to a modern stack

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,9 +7,9 @@ import { FaLocationArrow } from 'react-icons/fa'
 const Hero = () => {
   return (
     <div className="relative pb-20 pt-36">
-      <div className="absolute top-4 right-4 z-50">
-        <ModeToggle />
-      </div>
+      {/*<div className="absolute top-4 right-4 z-50">*/}
+      {/*  <ModeToggle />*/}
+      {/*</div>*/}
 
       <div>
         <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
